2b46326ac24902dc3f2f9be72bc0e5f96fd2d2ae
Jnarrr <lfebruary60@gmail.com>
2022-10-19 02:15:46 +0800

    ViewAppointmentWithLaravelAPI

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import AddClinic from './pages/AddClinic';
 import EditClinic from './pages/EditClinic';
 import VetSignup from './pages/VetRegister';
 import VetLogin from './pages/VetLogin';
+import ViewAppointment from './pages/ViewAppointment';
 
 import axios from 'axios';
 axios.defaults.baseURL = "http://localhost:8000/";
@@ -24,6 +25,7 @@ function App() {
             <Route path="/edit-clinic/:id" component={EditClinic} />
             <Route path="/VetRegister" component={VetSignup} />
             <Route path="/VetLogin" component={VetLogin} />
+            <Route path="/Appointment" component={ViewAppointment} />
 
           </Switch>
         </Router>
---
+++ src/pages/ViewAppointment.js
@@ -0,0 +1,83 @@
+import React, {useState, useEffect, Component} from 'react';
+import {Link, useHistory} from 'react-router-dom';
+
+import axios from 'axios';
+
+class ViewAppointment extends Component
+{
+    state = {
+        appointments: [],
+        loading: true,
+    }
+    async componentDidMount(){
+        const res = await axios.get('http://localhost:8000/api/appointments');
+        
+        if (res.data.status === true)
+        {
+            this.setState({
+                appointments: res.data.appointments,
+                loading: false,
+            });
+        }
+    }
+
+    render(){
+
+        var appointments_table = "";
+        if (this.state.loading)
+        {
+            appointments_table = <tr><td colSpan="6"> <h2>Loading...</h2> </td></tr>;
+        }
+        else 
+        {
+            appointments_table = 
+            this.state.appointments.map( (item) => {
+                return(
+                    <tr key={item.id}>
+                        <td>{item.id}</td>
+                        <td>{item.procedure}</td>
+                        <td>{item.date}</td>
+                        <td>{item.time}</td>
+                        <td>{item.pet}</td>
+                    </tr>
+                );
+            } )
+        }
+
+        return(
+            <div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12">
+                        <div className="card">
+                            <div className="card-header">
+                                <h4>Appointments</h4>
+                            </div>
+                            <div className="card-body">
+                                
+                                <table className="table table-bordered table-striped">
+                                    <thead>
+                                        <tr>
+                                            <th>ID</th>
+                                            <th>Procedures</th>
+                                            <th>Date</th>
+                                            <th>Time</th>
+                                            <th>Pet</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {appointments_table}
+                                    </tbody>
+                                </table>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        );
+    }
+}
+
+export default ViewAppointment;
